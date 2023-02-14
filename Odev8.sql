f49465363e128d817da332dfe2ae75a89acb2393
Mehmet Can Duru <mehmetcan60733@gmail.com>
2023-02-14 12:20:37 +0300

    added Odev8 and changed README

---
+++ Odev8.sql
@@ -0,0 +1,75 @@
+--CASE1
+
+CREATE TABLE employee(
+
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50),
+	birthday DATE,
+	email VARCHAR(50)
+);
+
+--CASE2
+
+insert into employee (id, name, birthday, email) values (1, 'Mariam', '2004-07-18', 'mdorber0@ibm.com');
+insert into employee (id, name, birthday, email) values (2, 'Sophey', '1996-07-30', 'sisack1@upenn.edu');
+insert into employee (id, name, birthday, email) values (3, 'Thurstan', '1982-02-23', 'taxup2@wikimedia.org');
+insert into employee (id, name, birthday, email) values (4, 'Thomasine', '1998-09-01', 'tgosz3@paypal.com');
+insert into employee (id, name, birthday, email) values (5, 'Karim', '1983-08-19', 'kcosgrove4@google.com.br');
+insert into employee (id, name, birthday, email) values (6, 'Benoit', '1982-06-11', 'bsummergill5@blog.com');
+insert into employee (id, name, birthday, email) values (7, 'Howey', '1996-07-29', 'hgrier6@japanpost.jp');
+insert into employee (id, name, birthday, email) values (8, 'Adelaida', '1987-04-25', 'abrosel7@kickstarter.com');
+insert into employee (id, name, birthday, email) values (9, 'Elijah', '1990-03-11', 'eshingler8@huffingtonpost.com');
+insert into employee (id, name, birthday, email) values (10, 'Marleah', '1993-02-15', 'msplevings9@ucla.edu');
+insert into employee (id, name, birthday, email) values (11, 'Neal', '1987-10-16', 'nselesnicka@geocities.jp');
+insert into employee (id, name, birthday, email) values (12, 'Candide', '1993-10-19', 'cgalleyb@instagram.com');
+insert into employee (id, name, birthday, email) values (13, 'Linn', '1993-12-20', 'lwillbrahamc@google.co.uk');
+insert into employee (id, name, birthday, email) values (14, 'Jocelyne', '1996-10-25', 'jcolliholed@washingtonpost.com');
+insert into employee (id, name, birthday, email) values (15, 'Gannie', '2001-08-04', 'gmanaghe@alexa.com');
+insert into employee (id, name, birthday, email) values (16, 'Ethelbert', '2008-09-19', 'evanichevf@sphinn.com');
+insert into employee (id, name, birthday, email) values (17, 'Gilligan', '2002-02-23', 'granbyg@ezinearticles.com');
+insert into employee (id, name, birthday, email) values (18, 'Mylo', '1997-07-14', 'mjowersh@telegraph.co.uk');
+insert into employee (id, name, birthday, email) values (19, 'Barbette', '1993-05-02', 'bharbardi@nasa.gov');
+insert into employee (id, name, birthday, email) values (20, 'Mendel', '2009-01-16', 'mtillotj@sakura.ne.jp');
+insert into employee (id, name, birthday, email) values (21, 'Sharline', '2006-11-21', 'spfliegerk@blinklist.com');
+insert into employee (id, name, birthday, email) values (22, 'Timmy', '2004-06-07', 'tvalentinel@creativecommons.org');
+insert into employee (id, name, birthday, email) values (23, 'Iago', '2003-06-27', 'ibenkam@netvibes.com');
+insert into employee (id, name, birthday, email) values (24, 'Tawnya', '1981-01-07', 'tschurcken@spotify.com');
+insert into employee (id, name, birthday, email) values (25, 'Bonny', '2007-02-18', 'bemblowo@ow.ly');
+insert into employee (id, name, birthday, email) values (26, 'Fiann', '1996-03-30', 'fgeneryp@yelp.com');
+insert into employee (id, name, birthday, email) values (27, 'Karon', '1996-05-15', 'kwitterickq@skyrock.com');
+insert into employee (id, name, birthday, email) values (28, 'Marjory', '1988-02-01', 'mgheraldir@stumbleupon.com');
+insert into employee (id, name, birthday, email) values (29, 'Karel', '2006-02-07', 'kseelbachs@macromedia.com');
+insert into employee (id, name, birthday, email) values (30, 'Dave', '1988-03-14', 'dvostt@miibeian.gov.cn');
+insert into employee (id, name, birthday, email) values (31, 'Noak', '1988-02-08', 'nburchettu@cnet.com');
+insert into employee (id, name, birthday, email) values (32, 'Gunner', '1980-02-28', 'gwitheringtonv@ebay.co.uk');
+insert into employee (id, name, birthday, email) values (33, 'Harmon', '2009-09-17', 'hcarsonw@wisc.edu');
+insert into employee (id, name, birthday, email) values (34, 'Doreen', '1984-04-07', 'dfurzeyx@linkedin.com');
+insert into employee (id, name, birthday, email) values (35, 'Marcos', '1985-06-12', 'mlenniey@yahoo.co.jp');
+insert into employee (id, name, birthday, email) values (36, 'Benny', '1996-06-13', 'bfennelowz@dagondesign.com');
+insert into employee (id, name, birthday, email) values (37, 'Franni', '1987-07-10', 'fmalser10@examiner.com');
+insert into employee (id, name, birthday, email) values (38, 'Axel', '2000-03-29', 'asaill11@ebay.co.uk');
+insert into employee (id, name, birthday, email) values (39, 'Winny', '2005-08-25', 'wbenduhn12@blogspot.com');
+insert into employee (id, name, birthday, email) values (40, 'Perceval', '1992-11-02', 'pdalesio13@ameblo.jp');
+insert into employee (id, name, birthday, email) values (41, 'Abigael', '1983-07-07', 'amcclaren14@blog.com');
+insert into employee (id, name, birthday, email) values (42, 'Kalindi', '2007-12-09', 'kghirardi15@altervista.org');
+insert into employee (id, name, birthday, email) values (43, 'Rosalynd', '2006-01-16', 'rogelbe16@lulu.com');
+insert into employee (id, name, birthday, email) values (44, 'Nadean', '1990-06-22', 'nwardingley17@cyberchimps.com');
+insert into employee (id, name, birthday, email) values (45, 'Jorie', '1984-10-17', 'jstanbridge18@google.com.hk');
+insert into employee (id, name, birthday, email) values (46, 'Andreana', '1981-09-16', 'asperski19@paypal.com');
+insert into employee (id, name, birthday, email) values (47, 'Louise', '1985-02-28', 'ltwelftree1a@ifeng.com');
+insert into employee (id, name, birthday, email) values (48, 'Roxanne', '1994-06-14', 'rfishbourne1b@washington.edu');
+insert into employee (id, name, birthday, email) values (49, 'Eldin', '2009-02-06', 'elambshine1c@naver.com');
+insert into employee (id, name, birthday, email) values (50, 'Andreas', '2005-07-20', 'adefraine1d@reuters.com');
+
+--CASE3
+
+UPDATE employee
+SET name = 'XXXX',
+	birthday = '1900-10-10',
+	email = 'xxxx@yyyy.com.tr'
+WHERE id IN(1,2,3,4,5);
+
+--CASE4
+
+DELETE FROM employee
+WHERE id <= 5;
